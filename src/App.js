ee19b5f752aa602a2ac0f0e8bc7d3c0739be54e3
Loquen Jones <lrjones416@gmail.com>
2020-02-16 11:26:40 -0800

    basic react-player and quiz form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactPlayer from 'react-player';
+import PostVideoQuiz from './PostVideoQuiz';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        eSpark take home challenge
       </header>
+      <div className='main'>
+        <ReactPlayer 
+          url='https://www.youtube.com/watch?v=mRdMYuNeAng' 
+          controls={true}  
+        />
+        <PostVideoQuiz />
+      </div>
     </div>
   );
 }
---
+++ src/PostVideoQuiz.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function PostVideoQuiz(props) {
+  return (
+    <div className='quiz'>
+      <p>Question 1:</p>
+      <div>
+        <input type='textArea'/>
+        <button>Submit</button>
+      </div>
+    </div>
+  );
+}
+
+export default PostVideoQuiz;
