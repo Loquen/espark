d06c9b774cc8c7d93c5492fd4c6fc858eb384619
Loquen Jones <lrjones416@gmail.com>
2020-02-16 12:34:58 -0800

    track plays and pause times, render quiz on finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
 import PostVideoQuiz from './PostVideoQuiz';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        eSpark take home challenge
-      </header>
-      <div className='main'>
-        <ReactPlayer 
-          url='https://www.youtube.com/watch?v=mRdMYuNeAng' 
-          controls={true}  
-        />
-        <PostVideoQuiz />
+class App extends Component {
+
+  state = {
+    plays: 0,
+    duration: 0,
+    pauseTime: 0,
+    timesPaused: [],
+    showQuiz: false
+  }
+
+  handlePause = () => {
+    let newTimesPaused = this.state.timesPaused;
+    newTimesPaused.push(this.state.pauseTime);
+    this.setState({ timesPaused: newTimesPaused });
+  }
+
+  // STILL NEED TO FIX so it updates every new play
+  // We can change it to handlePlay and only increment when pauseTime < 1
+  handlePlay = () => {
+    if(this.state.pauseTime < 1){
+      let plays = this.state.plays;
+      this.setState({plays: plays + 1});
+    }
+  }
+
+  handleProgress = state => {
+    this.setState({pauseTime: state.playedSeconds})
+  }
+
+  handleEnded = () => {
+    this.setState({showQuiz: true});
+  }
+
+  handleDuration = (duration) => {
+    this.setState({ duration })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          eSpark take home challenge
+        </header>
+        <div className='main'>
+          <ReactPlayer 
+            url='https://www.youtube.com/watch?v=mRdMYuNeAng' 
+            controls={true}  
+            onDuration={this.handleDuration}
+            onPlay={this.handlePlay}
+            onPause={this.handlePause}
+            onProgress={this.handleProgress}
+            onEnded={this.handleEnded}
+          />
+          {
+            this.state.showQuiz 
+            ? <PostVideoQuiz />
+            : null
+          }
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
